494b1969fd1d293a71b3ccc4ae468bd42676caec
x7peeps <421614066@qq.com>
2023-08-06 13:29:43 +0800

    hugo update commit, new pages up.

--- index.search.js
+++ index.search.js
@@ -59,6 +59,13 @@ var relearn_search_index = [
     "content": "",
     "description": "",
     "tags": null,
+    "title": "机器学习",
+    "uri": "/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
     "title": "工具",
     "uri": "/%E5%B7%A5%E5%85%B7/index.html"
   },
@@ -66,8 +73,29 @@ var relearn_search_index = [
     "content": "",
     "description": "",
     "tags": null,
-    "title": "机器学习",
-    "uri": "/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/index.html"
+    "title": "Tag - AI,claude",
+    "uri": "/tags/AIclaude/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "claude",
+    "uri": "/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/claude/index.html"
+  },
+  {
+    "content": "claude注册 claude免费，平替chatgpt\n注册账号 进入https://www.anthropic.com/product\n滚动到页面底部，点击Product News，“Claude in Slack”。这里最新claude2，https://www.anthropic.com/index/claude-2。\n点击“Add to Slack” 会进入Slack官网，https://slack.com/apps/A04KGS7N9A8-claude?tab=more_info 在这个页面上点击“登录并安装”\n这里需要先注册一个Slack账号，可以直接使用Apple ID登录，或者用邮件注册一个（可以用hotmail或者outlook，亲测sina邮箱不行）。 遇到了无法Authorization hasn’t been set up for this app, so you won’t be able to install it. 申请加入白名单 https://www.anthropic.com/earlyaccess 申请成功就可以添加到工作区\n注册成功登录后，需要创建新工作区\n输入团队名称、你的名字及团队成员后，就可以进入工作区，胜利就在眼前。\n进入工作区\n但是我们发现并没有Claude应用，因为我们只是完成了首次登录，还没有添加Claude到Slack中。再次登录“第三步”中的Slack官网，已经识别到你的工作区，这时点击“添加到Slack”。使用第三步中的地址：https://slack.com/apps/A04KGS7N\n出现不支持当前所在区域的提示，通过“科学”的方法更换你的IP地址，例如可以在百度中搜索“IP”查看你当前的IP地址：\n再次尝试“添加到Slack”见第七步，不出意外的成功！\n再次进入工作区，可以看到Claude应用，并且后续正常上网即可使用。工作区：https://app.slack.com/client/\n通过Claude应用发起沟通即可，实现ChatGPT完美平替\n参考： https://zhuanlan.zhihu.com/p/623242552\n",
+    "description": "",
+    "tags": "AI,claude",
+    "title": "claude注册",
+    "uri": "/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/claude/claude%E6%B3%A8%E5%86%8C/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
   },
   {
     "content": "【译】取证调查：虚拟内存pagefile.sys 原文：https://www.hackingarticles.in/forensic-investigation-pagefile-sys/\n在这片文章中，我们将学习如何在虚拟内存文件中进行取证调查。这里有很多信息能用于在内存镜像中提取有价值的信息。当然，更多的你还可以直接通过虚拟内存分析而不分析内存镜像。\n在设备上有很多包含很多内存片段的记录。这些文件是pagefile.sys,swapfile.sys和hiberfil.sys。我们将对pagefile.sys进行分析。\n目录 介绍 使用FTK imager获取内存和虚拟内存 使用Belkasoft Evidence Centre分析 介绍 pagefile.sys也指在windows操作系统中被用于swap交换文件或者虚拟内存文件存储超出物理内存容量部分的信息。pagefile.sys在windows操作系统的位置位于 C:\\pagefile.sys。windows操作系统最高支持16个分页文件；目前只有一个被使用。\n无论处于什么目的，当你在Windows中打开一个应用，你的PC将会用到RAM内存。当你打开更多的应用超出了PC内存能够容纳的范围，在内存中运行的程序将会被移动到虚拟内存中。这就是分页或者意味着分页文件被用作强化了内存，也被称为虚拟内存。\n使用获取内存和虚拟内存 我们将使用FTK imager获取虚拟内存pagefile.sys。\nFTK imager是一个镜像和数据查看工具，FTK imager通常创建系统镜像（取证镜像）电子证据。你可以在这里下载FTK imager（https://www.exterro.com/ftk-imager）\n点击capture memory创建内存镜像。 下一步是选择保存的目录，勾选”include pagefile“，点击获取内存capture memory。 内存获取进程将会在点击capture memory按钮之后开始。 进程结束后，内存镜像和虚拟内存就会被写在之前选定的目录中。 使用Belkasoft Evidence Centre分析 现在我们来Belkasoft Evidence Centre分析写入pagefile.sys。Belkasoft Evidence Centre是一个集成获取、分析和刻入数字证据的分析工具。你可以在这里下载免费版本的工具（https://belkasoft.com/get）\n首先，我们创建个新的case。填写case信息，选择有管理权限的文件夹，如果你想的话，你还可以添加一个case的描述。点击“create and open”执行分析。 用于分析虚拟内存文件，选择配置RAM image内存镜像；添加使用FTK imager镜像的pagefile.sys文件。\n选择想要的数据类型进行搜索。这里有大量的数据类型可以扫描。点击完成按钮即可。 完成了上述步骤后会显示这样一个面板。展示了正确的关于虚拟内存的统计数据信息。总数1097个文件被识别，其中包括URLs、图片和其他文件。 在这个case explorer标签在dashboard标签旁边可以扩展和查看每个概要文件列。数据被切分为浏览器、图片、系统文件和其他文件。 让我们展开分析Browser配置文件。让我们检查chrome切分部分的更多细节，其中之一被分为chrome历史记录包含了URLs。其中高亮的部分就是历史访问的URL地址列。\n另一个浏览器配置是opera浏览器，分析opera部分的内容也是同样，其中展示了URL浏览记录。 从配置文件切分的数据也包含了一些图片信息。这些图片是我访问过的网站的图片或者是其他缩略图。 belkasoft evidence center的一个非常好的特性是，它允许你简单在图片上按右键，可以进行分析各方面，例如检查皮肤、图片中的色情信息，特定的文字或者脸。所有的这些特性都在分析的时候会起到比较有用的效果。 一些系统文件也被从虚拟内存中切分，显示了NetBios名字，文件地址，文件大小等数据。 时间轴标签展示了数据切分的整体情况可以通过时间和URL来进行简单的分析历史访问的网站。 search results标签展示搜索结果概要的工具。下面的截图展示了搜索引擎返回的带有link和配置文件名的结果。 相同的，你可以对休眠文件进行取证调查。使用FTK 在系统C:/hiberfile.sys目录下扩展hiberfil.sys（用于存储系统休眠模式下存储的信息）进行取证，并且使用Belkasoft Evidence Centre进行分析。\n虚拟内存文件的分析对于浏览器鉴定有很大帮助。\n",
@@ -82,13 +110,6 @@ var relearn_search_index = [
     "tags": null,
     "title": "0x03取证分析",
     "uri": "/%E5%BA%94%E6%80%A5%E5%93%8D%E5%BA%94/0x03%E5%8F%96%E8%AF%81%E5%88%86%E6%9E%90/index.html"
-  },
-  {
-    "content": "",
-    "description": "",
-    "tags": null,
-    "title": "Tags",
-    "uri": "/tags/index.html"
   },
   {
     "content": "",
@@ -819,11 +840,11 @@ var relearn_search_index = [
     "uri": "/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/chatgpt/index.html"
   },
   {
-    "content": "",
+    "content": "chatgpt对接微信服务搭建 背景，这里有个需求就是我希望在日常微信的某个群中使用chatgpt回答群里的日常问题。如果你也想搭建一个对接微信的chatgpt问答机器人那么可以参考本文。\n这里使用chatgpt-on-wechat项目进行搭建，项目地址： https://github.com/zhayujie/chatgpt-on-wechat\n安装过程 git config –global –unset http.proxy git clone https://github.com/zhayujie/chatgpt-on-wechat.git pip3 install -r requirements.txt pip3 install -r requirements-optional.txt pip3 install azure-cognitiveservices-speech 修改config.json配置 windows: pip3 install SpeechRecognition windows注意：在执行的时候要使用git bash类linux的命令行防止unicode报错 ffmpeg安装 http://ffmpeg.org/ 配置 https://zhuanlan.zhihu.com/p/118362010 运行python3 app.py bug 编码问题 Q: UnicodeEncodeError: ‘gbk’ codec can’t encode character ‘\\u2580’ in position 0: illegal multibyte sequence A: 在app.py文件中加入 import io sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码 目前启动的时候每次都要手动登录才行，不定期会自动退出登录需要重新登录，尝试解决。\n",
     "description": "",
     "tags": null,
-    "title": "claude",
-    "uri": "/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/claude/index.html"
+    "title": "chatgpt微信机器人搭建",
+    "uri": "/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0/chatgpt/chatgpt%E5%BE%AE%E4%BF%A1%E6%9C%BA%E5%99%A8%E4%BA%BA%E6%90%AD%E5%BB%BA/index.html"
   },
   {
     "content": "Clonezilla：https://clonezilla.org/ 多平台的分区和磁盘克隆程序。\nClonezilla是一个分区和磁盘成像/克隆程序，类似于True Image®或Norton Ghost®。帮助您进行系统部署、裸金属备份和恢复。Clonezilla有三种类型:Clonezilla live、Clonezilla lite server和Clonezilla SE(服务器版)。Clonezilla live适用于单机备份和恢复。虽然Clonezilla lite server或SE是用于大规模部署的，但它可以同时克隆许多(40多台!)计算机。Clonezilla只保存和恢复硬盘中使用过的块。这提高了克隆效率。在42节点集群中的一些高端硬件中，理论可达8gb/min速率恢复速率。\n特性 支持许多文件系统:\n(1)ext2、ext3, ext4, reiserfs, reiser4, xfs、jfs, btrfs, f2fs nilfs2 GNU / Linux, (2) FAT12, FAT16, FAT32, NTFS Windows, (3)HFS + Mac OS, (4) UFS FreeBSD, NetBSD, OpenBSD, (5) minix minix, (6) VMFS3 VMFS5 VMWare ESX。 因此，无论32位(x86)操作系统还是64位(x86-64)操作系统，都可以克隆GNU/Linux、MS windows、intel Mac OS、FreeBSD、NetBSD、OpenBSD、Minix、VMWare ESX、Chrome OS/Chromium操作系统。对于这些文件系统，Partclone只保存和恢复分区中使用过的块。对于不支持的文件系统，在Clonezilla中，扇区到扇区的复制是由dd完成的。\n支持GNU/Linux下的LVM2 (LVM版本1不支持)。 可以重新安装引导加载程序，包括grub(版本1和版本2)和syslinux。 支持MBR和GPT分区格式的硬盘驱动器。Clonezilla live也可以在BIOS或uEFI机器上启动。 支持无人值守模式。几乎所有的步骤都可以通过命令和选项来完成。您还可以使用许多引导参数来定制您自己的映像和克隆。 支持一个映像恢复到多个本地设备。 镜像可以被加密。这是通过ecryptfs完成的，这是一种符合posix的企业加密堆叠文件系统。 Clonezilla SE支持多播，适合大规模克隆。如果您的客户端支持PXE和局域网唤醒，您也可以远程使用它来保存或恢复一堆计算机。 Clonezilla lite服务器支持BT (Bittorrent)，适合大规模部署。BT模式的工作由Ezio完成。 映像文件可以在本地磁盘、ssh服务器、samba服务器、NFS服务器或WebDAV服务器上。 AES-256加密可用于保护数据的访问、存储和传输。 基于Partclone(默认)、Partimage(可选)、ntfsclone(可选)、dd对分区进行镜像或克隆。然而，包含其他一些程序的Clonezilla不仅可以保存和恢复分区，还可以保存和恢复整个磁盘。 通过使用我们自己开发的另一款免费软件drbl-winroll，可以自动更改克隆的MS windows机器的主机名、组名和SID。 最小运行环境 Clonezilla live所需最小系统环境：\n处理器为X86或X86 -64\n196mb系统内存(RAM)\n启动设备，如CD/DVD驱动器、USB端口、PXE或硬盘驱动器\n局限性 目标分区必须等于或大于源分区。 差异/增量备份尚未实现。 在线成像/克隆还没有实现。必须卸载要映像或克隆的分区。 由于图像格式的限制，无法探索或装入图像。您可以从映像中恢复单个文件。但是，您仍然有办法做到这一点，请阅读本文。 恢复Clonezilla live与多个cd或dvd还没有实现。现在，如果你选择创建恢复iso文件，所有的文件都必须在一个CD或DVD中。 Clonezilla版本 Clonezilla Live: Clonezilla Live允许你使用CD/DVD或USB闪存驱动器引导和运行Clonezilla(仅提供单播) **Clonezilla lite服务器:**Clonezilla lite服务器允许你使用Clonezilla live进行大规模克隆(支持单播，广播，多播，bt) Clonezilla SE: DRBL包含了Clonezilla SE，因此为了使用Clonezilla进行大规模克隆(支持单播、广播和组播)，必须首先建立DRBL服务器。 这里因为日常使用的clonezille live会比较多这里主要介绍live。\nClonezilla Live及其使用 https://clonezilla.org/clonezilla-live.php\nClonezilla Live是一个针对x86/amd64 (x86-64)计算机的小型可引导GNU/Linux发行版。Clonezilla SE(服务器版)从2004年开始开发，用于同时部署多台计算机。这是一个非常有用的工具，但是它也有一些限制。为了使用它，您必须首先准备一个DRBL服务器，并且要部署的机器必须从网络启动(例如PXE/iPXE)。\n如何安装Clonezilla Live 要安装Clonezilla live，基本步骤是下载预构建的Clonezilla live，然后将其放入引导介质(CD、USB闪存驱动器或USB硬盘驱动器)。有两种类型的文件可用，iso和zip。前者用于CD，后者用于USB闪存驱动器。此外，您还可以将Clonezilla放在硬盘驱动器或PXE服务器上。\nCD / DVD: 下载一个ISO文件光盘/DVD。然后你可以烧录文件CD / DVD与任何烧程序,这种K3b GNU / Linux或InfraRecorder女士的窗户,和记得选择“烧录镜像”的燃录到CD, 该CD可以用来引导机器启动镜像或克隆。在这里可以找到关于使用InfraRecorder创建Clonezilla live CD的分步文档。\nUSB闪存驱动器或USB硬盘: 要把Clonezilla放到u盘或u盘上，请查看这个文档。\n硬盘: 要将Clonezilla安装在已经安装了操作系统的硬盘上，请检查这个文档。\nPXE服务器: 要将Clonezilla运行在PXE服务器上并通过PXE引导您的客户机，请检查这个文档。\n如果您有兴趣从头创建Clonezilla live iso或zip文件，请检查这个文档。//创建Clonezilla live 2.x!\n如何使用Clonezilla Live Please refer to this doc for more details. 官方文档\n下面是官方手册（已翻译）：\n下面是Clonezilla Live启动的菜单：\n第一个页面是Clonezilla Live默认的模式界面，它将默认为分辨率为1024x768的帧缓冲模式。\n在第二选项“Other modes of Clonezilla live”中还有更多的模式可供选择，如:800X600或640x480，如果你愿意，如下图所示:\n选择“Clonezilla live (To RAM。引导介质可以稍后删除)，与第一个相同的功能，不同的是当Clonezilla live引导完成时，所有必要的文件被复制到内存，可以移除启动介质(CD或u盘)。\n如果您不需要中文或日文版环境，或者您的计算机在帧缓冲模式下遇到问题，您可以选择“Clonezilla Live (no framebuffer)”在英文环境中克隆。\n选择“Clonezilla live(故障安全模式)”，是为了当你不能启动你的机器时出现问题，比如你的机器的ACPI在内核中不受支持。\n如果你想在你的硬盘上启动本地操作系统，你可以选择“本地操作系统在硬盘(如果可用)”。这是引导介质中的一个额外功能，与Clonezilla Live没有任何关系。\n选择“FreeDOS”，可以让你引导你的机器进入自由 DOS。这是引导介质中的一个额外功能，与Clonezilla Live没有任何关系。\n选择“使用Memtest86+进行内存测试”是为了使用Memtest86+进行内存测试。这是引导介质中的一个额外功能，与Clonezilla Live没有任何关系。\n选择“通过etherboot进行网络引导”或“通过gpxe进行网络引导”，可以通过etherboot或gpxe进行网络引导。如果您的计算机没有PXE网络，您可以使用它从网络引导。这是引导介质中的一个额外功能，与Clonezilla Live没有任何关系。\n在选择语言和键映射之后，首先会提示您分配映像存储库，它可以是本地磁盘、ssh服务器、nfs服务器、samba服务器(网络邻居)或任何可以作为/home/ partimag挂载的文件系统。一旦存储库就绪，您就可以继续保存或恢复映像。请按照屏幕上的说明操作。关于Clonezilla live的两个主要截图: ///注意/// Clonezilla镜像名实际上就是目录名。例如，如果你将镜像名命为2007-05-NOVISTA，所有的信息将会被保存在/home/partimag/2007-05-NOVISTA/目录中。MBR引导,切分表以及以及切分文件都会被储存。因此当你想要还原镜像的时候，应挂载/home/partimag到对应磁盘和路径，/home/partimag/这个目录下，并且还要有2007-05-NOVISTA目录。\nClonezilla Live prepare ocsroot菜单\n[专家模式]如果你想手动挂载clonezilla image home (/home/partimag)，遵循以下步骤:\n运行“sudo su -”成为root。 您必须准备另一个可写的设备或空间，并将其挂载为/home/partimag(注意!如果你的引导媒体是可写的,如USB闪存驱动器或您选择使用内存选项,已有/home/partimag与/live_media /home /partimag,你必须删除该文件,并创建一个目录/home/partimag作为挂载点“rm - f /home/partimag;mkdir - p /home/partimag)。例如，如果你想使用Clonezilla来保存/dev/hda，并将映像放在/dev/hdb1中，那么你必须通过“mount -t auto /dev/hdb1 /home/partimag”将/dev/hdb1挂载为/home/partimag。 如果/dev/hdb1的文件系统是ntfs，你必须使用\"ntfs-3g /dev/hdb1 /home/partimag\"来挂载它，这样它就可以写了。请记住，还支持sshfs和smbfs。例如，使用sshfs，你可以挂载你的远程ssh服务器:\nsshfs ACCOUNT@SSH_SERVER: / ABSOLUTE_PATH /home/partimag例如，如果你想挂载你的ssh服务器192.168.100.254目录/work/pool，像这样运行:\n/home/partimag sshfs root@192.168.100.254: /home/partimag如果你想用目录/work/smb挂载你的samba服务器192.168.200.254，像这样运行:\nmount -t cifs -o user=your_user_name //192.168.200.254/work/smb /home/partimag . conf . mount -t cifs -o user=your_user_name //192.168.200.254/work/smb /home/partimag . conf如果您想用目录/work/smb挂载samba服务器192.168.200.254，用户名为“administrator”，密码为“pass”，请像这样运行:\nmount -t cifs -o user=administrator,password=pass //192.168.200.254/work/smb /home/partimag . mount -t cifs -o user=administrator,password=pass///说明///上述命令中，Linux下不使用smbfs，LINUX下使用cifs协议，cifs协议对windows MS文件共享的兼容性更好**。当您使用smbfs时，Clonezilla live将无法保存正确的镜像!**\n运行“clonezilla”来使用clonezilla，然后按照屏幕上的说明操作。 更多文章示例 http://clonezilla.nchc.org.tw/related_article/\n通过视频学习了解clonezilla使用 通过视频学习如何使用clonezilla：\nhttps://www.youtube.com/watch?v=cEE_vn8E0Kk\nhttps://www.youtube.com/results?search_query=clonezilla\n相关视频链接\nBackup with Clonezilla | LAS | s25e10 How To Backup AND Restore you Computer with CloneZilla! (BEST HD Tutorial!!) Disk to Disk Copy with Clonezilla How to Do a System Backup Using Clonezilla How to use CloneZilla Tutorial (Making an image file and copying it to another computer) Clonezilla - Backup Linux/Ubuntu System Clonezilla video tutorial (italian) ",
