36b6626e9ebe6085443f1cf9fe861cf05d4bd6ad
x7peeps <xtpeeps@gmail.com>
2026-06-10 11:34:37 +0800

    docs: 更正 CVE-2025-27363 为 FreeType 越界写入漏洞分析

--- hugo-src/static/js/custom.js
+++ hugo-src/static/js/custom.js
@@ -234,3 +234,11 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+
+// Interactive Global Spotlight
+window.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('mousemove', (e) => {
+        document.body.style.setProperty('--mouse-x', `${e.clientX}px`);
+        document.body.style.setProperty('--mouse-y', `${e.clientY}px`);
+    });
+});
